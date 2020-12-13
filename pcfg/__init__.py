373666b54261e51e3a8877966e16ad3792503d87
Thomas Breydo <tbreydo@gmail.com>
2020-12-13 01:43:25 -0500

    Update versioning method

--- pcfg/__init__.py
+++ pcfg/__init__.py
@@ -1,2 +1,11 @@
-__version__ = "0.1.0"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except importlib_metadata.PackageNotFoundError:
+    __version__ = None
+
 from .pcfg import PCFG
