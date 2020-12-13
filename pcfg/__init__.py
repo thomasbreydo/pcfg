037aabea93cf59e83cbafb8917e291f1486d2ad7
Thomas Breydo <tbreydo@gmail.com>
2020-12-13 02:00:42 -0500

    Update spacing of __init__.py

--- pcfg/__init__.py
+++ pcfg/__init__.py
@@ -2,7 +2,6 @@ try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
     import importlib_metadata
-
 try:
     __version__ = importlib_metadata.version(__name__)
 except importlib_metadata.PackageNotFoundError:
